9e69d2bb1e7f790a12162ae86d7208a906ec15de
christianquispe <christianquispecamasca@gmail.com>
2021-10-13 22:34:36 -0500

    chore: setup nextjs project

---
+++ src/Layout/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+interface LayoutProps {
+	title: string
+}
+
+const Layout: React.FC<LayoutProps> = ({ children, title }) => {
+	return (
+		<div>
+			<Head>
+				<title>{title}</title>
+			</Head>
+			{children}
+		</div>
+	)
+}
+export default Layout
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../Layout";
+
+const Home = () => {
+  return (
+    <Layout title="Christian Quispe">
+      <span>Home</span>
+    </Layout>
+  );
+};
+
+export default Home;
