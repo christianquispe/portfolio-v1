ef041d1113886129334bd1d4685517a7e1649326
christianquispe <christianquispecamasca@gmail.com>
2021-10-13 22:47:47 -0500

    chore: setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{js, ts, tsx, jsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
