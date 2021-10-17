bb5a97797c022bb62999a5530057167e90c418c0
christianquispe <christianquispecamasca@gmail.com>
2021-10-16 23:13:26 -0500

    chore: add section component

---
+++ src/Components/Section.tsx
@@ -0,0 +1,23 @@
+interface SectionProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLElement>,
+    HTMLElement
+  > {
+  bg: 'gray' | 'white';
+}
+
+const Section: React.FC<SectionProps> = (props) => {
+  const { children, className, bg = "white", ...rest } = props;
+
+  return (
+    <section
+      className={`py-14 px-4 lg:p-20${
+        className ? ` ${className}` : ""
+      }${bg === 'gray' ? ' bg-gray-100' : ''}`}
+      {...rest}
+    >
+      {children}
+    </section>
+  );
+};
+export default Section;
