a76729856789dc8f0a9ec849af815b1eb0a202f1
christianquispe <christianquispecamasca@gmail.com>
2021-10-16 23:15:05 -0500

    chore: update layout component

--- src/Layout/NavBar/NavBar.tsx
+++ src/Layout/NavBar/NavBar.tsx
@@ -5,33 +5,51 @@ const NavBar: React.FC = (props) => {
   const { children } = props;
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div className="flex items-center flex-shrink-0 mr-6">
-        <span className="text-xl tracking-tight">Christian</span>
-      </div>
-      <div className="block lg:hidden">
-        <button
-          onClick={() => setCollapse(!collapse)}
-          className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400"
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+    <>
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-4">
+        <div className="block">
+          <img
+            className="Logo"
+            src="/icons/logo.svg"
+            alt="Christian Quispe logo"
+          />
+        </div>
+        <div className="block lg:hidden">
+          <button
+            onClick={() => setCollapse(!collapse)}
+            className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400"
           >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
-      <ul
-        className={`lg:flex lg:items-center text-sm md:text-base lg:w-max${
-          collapse ? " block w-full" : " hidden"
-        }`}
-      >
-        {children}
-      </ul>
-    </nav>
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <ul
+          className={`lg:flex lg:items-center text-sm md:text-base lg:w-max${
+            collapse ? " block w-full" : " hidden"
+          }`}
+        >
+          {children}
+        </ul>
+      </nav>
+      <style jsx>{`
+        .Logo {
+          width: 60px;
+          height: 60px;
+        }
+        @media screen and (min-width: 768px) {
+          .Logo {
+            width: 70px;
+            height: 70px;
+          }
+        }
+      `}</style>
+    </>
   );
 };
 export default NavBar;
--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 import { NavBar, Item as NavItem } from "./NavBar";
 
@@ -10,13 +11,26 @@ const Layout: React.FC<LayoutProps> = ({ children, title }) => {
   return (
     <>
       <header className="container mx-auto">
-				<NavBar>
-					<NavItem>About me?</NavItem>
-					<NavItem>Portfolio</NavItem>
-					<NavItem>Contact</NavItem>
-				</NavBar>
+        <NavBar>
+          <NavItem>
+            <Link href="#about">
+              <a>About me?</a>
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link href="#about">
+              <a>Portfolio</a>
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link href="#about">
+              <a>Contact</a>
+            </Link>
+          </NavItem>
+        </NavBar>
         <Head>
           <title>{title}</title>
+          <link rel="icon" href="/icons/logo.svg" type="image/x-icon" />
         </Head>
       </header>
       {children}
