8c65c33a43e3dfcbda514837af25b054ea6ab06b
christianquispe <christianquispecamasca@gmail.com>
2021-11-06 00:36:56 -0500

    fix: sizes os icons

--- src/icons/SvgSpain.tsx
+++ src/icons/SvgSpain.tsx
@@ -3,14 +3,14 @@ import * as React from "react";
 function SvgSpain(props) {
   return (
     <svg
-      width={512}
-      height={512}
+      width={23}
+      height={17}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M32 96h448v320H32V96z" fill="#E92B2B" />
-      <path d="M32 200h448v112H32V200z" fill="#FFD02B" />
+      <path d="M0 0h23v16.429H0V0z" fill="#E92B2B" />
+      <path d="M0 5.34h23v5.75H0V5.34z" fill="#FFD02B" />
     </svg>
   );
 }
