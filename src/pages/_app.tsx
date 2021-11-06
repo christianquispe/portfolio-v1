2119e59694568ce357f45fb696d1b803d33f71b4
christianquispe <christianquispecamasca@gmail.com>
2021-11-06 16:11:04 -0500

    chore: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,35 @@
 import { NextIntlProvider } from "next-intl";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import * as ga from "../lib/ga";
 
 // Tailwindcss
 // import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 
 // React-multi-carousel
-import 'react-multi-carousel/lib/styles.css'
+import "react-multi-carousel/lib/styles.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      console.log(url)
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <NextIntlProvider
       // To achieve consistent date, time and number formatting
