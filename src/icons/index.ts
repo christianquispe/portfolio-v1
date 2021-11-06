806632c6ac2cdc4106aa164305d431d003448c82
christianquispe <christianquispecamasca@gmail.com>
2021-11-06 00:58:04 -0500

    chore: add new svg icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,5 +1,15 @@
 import SvgJavascript from "./SvgJavascript";
 import SvgSpain from "./SvgSpain";
 import SvgUsa from "./SvgUsa";
+import SvgTypescript from "./SvgTypescript";
+import SvgExpress from "./SvgExpress";
+import SvgGraphql from "./SvgGraphql";
 
-export { SvgJavascript, SvgSpain, SvgUsa }
+export {
+  SvgJavascript,
+  SvgSpain,
+  SvgUsa,
+  SvgTypescript,
+  SvgExpress,
+  SvgGraphql,
+};
