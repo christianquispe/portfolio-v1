baacf8f0fee605d82f7eb42805078c7fa94dd532
christianquispe <christianquispecamasca@gmail.com>
2021-11-06 04:25:29 -0500

    chore: add cdn for recaptcha

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            src="https://www.google.com/recaptcha/api.js?&render=explicit"
+            async
+            defer
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
