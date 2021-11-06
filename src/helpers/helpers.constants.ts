e979c25e97e1ce64d5234236075acc392520af6d
christianquispe <christianquispecamasca@gmail.com>
2021-11-06 04:38:25 -0500

    fix: projects data

--- src/helpers/helpers.constants.ts
+++ src/helpers/helpers.constants.ts
@@ -15,20 +15,6 @@ const constants = {
         stack: ["React", "Typescript", "React Hooks"],
         img: "/projects/dely.png",
       },
-      {
-        name: "React context crud",
-        description: "Simple crud usando useContext",
-        url: "https://crud-react-context.vercel.app/",
-        stack: ["React", "Typescript", "React Hooks"],
-        img: "/projects/crud-use-context.png",
-      },
-      {
-        name: "Dely landing page",
-        description: "Landing page de la compañia Dely",
-        url: "https://dely.netlify.app/",
-        stack: ["React", "Typescript", "React Hooks"],
-        img: "/projects/dely.png",
-      },
     ],
   },
 };
