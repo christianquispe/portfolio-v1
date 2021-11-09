12c839d1d8f5c3fa8e269cbd18b9a4a513644db0
christianquispe <christianquispecamasca@gmail.com>
2021-11-09 09:10:01 -0500

    chore: implement buttom component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+const Button: React.FC = (props) => {
+  const { children } = props;
+
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      {children}
+    </button>
+  );
+};
+
+export default Button;
