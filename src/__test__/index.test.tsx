6411fdecafca698bd145e0bfe01b88c29fb5ade3
christianquispe <christianquispecamasca@gmail.com>
2021-11-09 05:06:04 -0500

    chore: add jest

---
+++ src/__test__/index.test.tsx
@@ -0,0 +1,5 @@
+import { sumar } from "../helpers/helper.functions";
+
+test("the best flavor is grapefruit", () => {
+  expect(sumar({ a: 2, b: 4 })).toBe(6);
+});
---
+++ src/helpers/helper.functions.ts
@@ -0,0 +1,3 @@
+export function sumar({ a, b }: { a: number, b: number }): number {
+	return a + b
+}
